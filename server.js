1a79058c0405a42b1a7b26ff51bb427445a70d89
Adrian West <adrianwest@Adrians-MBP.home>
2017-03-02 13:04:22 +0000

    fixing issue 1

--- server.js
+++ server.js
@@ -69,5 +69,5 @@ app.get('/bad', (req, res) => {
 
 
 app.listen(port, () => {                          //see package.json..Heroku needs the "start" script.
-  console.log(`ervder is up on port: ${port}`);
+  console.log(`server is up on port: ${port}`);
 });
